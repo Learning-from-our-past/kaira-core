d2a66fbd9b05d89e4a81fa96a5669c25d64c4eb5
Tuomas Salmi <salmi.tuomas@gmail.com>
2014-11-26 15:25:52 +0200

    Extraction process now saves all the error exceptions and related text to a separate errors.csv for studying. Also _newtags.xml has the same data as the _tags.xml but was generated with newer (and hopefully better!) regex from text editor. However, for some reason, it gives about 600 errors while older gave ~400. Needs investigation. Already identified some problems with names, dob regex and OCR-errors.

--- dataprocessing/extractData.py
+++ dataprocessing/extractData.py
@@ -4,37 +4,35 @@ import re
 import unicodecsv
 import unicodedata
 
-root = readData.getXMLroot("rintamamiehet8_tags.xml")
-
+#use regex to extract the person's names and birthday from given text
+#returns dict containing the data
+#Possible exception handling should be done in higher level.
 def extractPersonNameAndBirthday(text):
-    text = ' '.join(text.split())
-    print "----"
-
-    print text
+    text = ' '.join(text.split())   #remove excess whitespace and linebreaks
     p = re.compile(ur'\A([A-ZÄ-Ö]{3,}), {0,100}([A-ZÄ-Öa-zä-ö ]{0,}), {0,100}s {0,100}([0-9]{1,2} {0,3}\.[0-9]{1,2} {0,3}\.[0-9]{1,2})',re.UNICODE)
     m = p.match(unicode(text))
-    print
-    print m.group(1)
-    print m.group(3)
-
     return {"surname": m.group(1), "firstnames": m.group(2), "birthday": m.group(3)}
-
-print extractPersonNameAndBirthday(root[0].text)
 
 
 errors = 0
+root = readData.getXMLroot("rintamamiehet8_newtags.xml")
 #save the extract4ed info to a csv file:
 with open("soldiers.csv", "wb") as results:
-    writer = unicodecsv.writer(results, delimiter=";")
-    writer.writerow(["surname", "first names", "birthday"])
+    with open("errors.csv", "wb") as errorcsv:
+        writer = unicodecsv.writer(results, delimiter=";")
+        writer.writerow(["surname", "first names", "birthday"])
 
-    for child in root:
-        try:
-            d = extractPersonNameAndBirthday(child.text)
-            writer.writerow([d["surname"], d["firstnames"], d["birthday"]])
-        except:
-            errors +=1
-            continue
+        ewriter = unicodecsv.writer(errorcsv, delimiter=";")
+        ewriter.writerow(["Exception", "Entry text"])
+
+        for child in root:
+            try:
+                d = extractPersonNameAndBirthday(child.text)
+                writer.writerow([d["surname"], d["firstnames"], d["birthday"]])
+            except Exception as e:
+                ewriter.writerow([str(e), child.text])
+                errors +=1
+                continue
 
 
 print "Errors encountered: " + str(errors)
