aa39b6e4e7dba192eead14e52c311fd032e38fe8
Tuomas Salmi <salmi.tuomas@gmail.com>
2015-03-25 14:06:22 +0200

    - Empty xml-entries are removed from file when edited file is saved - Those removed entries are also removed from the list in the editor during saving. Is this a good idea...?

--- dataprocessing/qtgui/savefile.py
+++ dataprocessing/qtgui/savefile.py
@@ -4,11 +4,13 @@ from PyQt5.QtCore import pyqtSlot, QEvent, QSettings, QStandardPaths
 from lxml import etree
 from PyQt5.QtGui import QStatusTipEvent, QDesktopServices
 
+
 class SaveFile(QObject):
 
-    def __init__(self, parent):
+    def __init__(self, parent, dataEntries):
         super(SaveFile, self).__init__(parent)
         self.parent = parent
+        self.dataEntries = dataEntries
 
 
     def choose_place_to_save_xml(self):
@@ -22,10 +24,27 @@ class SaveFile(QObject):
             if filename[0] != "":
                 self._save_to_xml(self.parent.xmlDocument, filename[0])
 
-    def _save_to_xml(self,xmldata, path):
+    def _save_to_xml(self, xmldata, path):
         #write modifications to a new xml-file:
+        self.dataEntries = self.parent.dataEntries
+        self._remove_empty_elements(xmldata)
         print ("Kirjoitetaan ")
         f = open(path, 'wb')
         f.write(etree.tostring(xmldata, pretty_print=True, encoding='unicode').encode("utf8"))
         f.close()
-        print ("valmis")
+
+
+    def _remove_empty_elements(self, xmldocument):
+        for child in xmldocument:
+            if child.text.strip() == "":
+                xmldocument.remove(child)
+                print("lapsi pois")
+                print(len(self.dataEntries))
+                for i in range(0, len(self.dataEntries)):
+                    print(self.dataEntries[i]["xml"])
+                    if self.dataEntries[i]["xml"] == child:
+                        print("pop")
+                        self.dataEntries.pop(i)
+                        break
+        self.parent.updateEntriesListSignal.emit()
+
