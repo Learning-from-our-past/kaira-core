cb550e7a6b8c4c396a397f7469489f1f1a935b07
Tuomas Salmi <salmi.tuomas@gmail.com>
2017-02-04 23:54:14 +0200

    Clean up
    Reorganizing and fixing up cx_freeze.

--- dataprocessing/cx_setup.py
+++ dataprocessing/cx_setup.py
@@ -3,14 +3,16 @@ from cx_Freeze import setup, Executable
 # Dependencies are automatically detected, but it might need
 # fine tuning.
 buildOptions = dict(packages = ["re", "atexit", "sip", "lxml._elementpath", "lxml.etree", "PyQt5.QtCore"],
-                    excludes = [],
-                    icon = "qtgui/icon.ico")
+                    excludes = [])
 
 import sys
 base = 'Win32GUI' if sys.platform=='win32' else None
 
 executables = [
-    Executable('start.py', base=base, targetName = 'Kaira.exe')
+    Executable('start.py',
+               base=base,
+               targetName = 'Kaira.exe',
+               icon = "qtgui/icon.ico")
 ]
 
 setup(name='Kaira',
