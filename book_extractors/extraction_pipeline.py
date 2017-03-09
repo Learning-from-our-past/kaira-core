fcb1bfdeb08f4764dda8cbf3fca30c307cb40ba2
Tuomas Salmi <salmi.tuomas@gmail.com>
2017-03-09 20:27:20 +0200

    Use generic extraction pipeline with karelian books
    Created generic extraction pipeline which should be usable in all books
    and all possible sub pipelines.

---
+++ book_extractors/extraction_pipeline.py
@@ -0,0 +1,61 @@
+import re
+
+
+class ExtractionPipeline:
+
+    def __init__(self, extractor_configurations):
+        self._extractor_configurations = extractor_configurations
+
+    def _find_extractor_by_class_name(self, class_to_find, extractors):
+        return next(e for e in extractors if type(e).__name__ == class_to_find.__name__)
+
+    # FIXME: Refactor extractors so that Pipeline is not built for each and every person. Should speed up and save lots of memory
+    def _build_extraction_pipeline(self, entry, eLogger):
+        extractors = []
+
+        for config in self._extractor_configurations:
+            extractor = config['extractor_class'](entry, eLogger)
+            extractors.append(extractor)
+
+        return extractors
+
+    def process(self, entry, eLogger):
+        extractors = self._build_extraction_pipeline(entry, eLogger)
+        extraction_results = {}
+
+        # Replace all weird invisible white space characters with regular space
+        entry['text'] = re.sub(r"\s", r" ", entry['text'])
+
+        for ext in zip(self._extractor_configurations, extractors):
+            # FIXME: Because of how current implementation of extractors, these functions has to be called here. Not ideal.
+            # Instead figure out a way to save match positions to pipeline level outside of extractor class and make extractors stateless.
+            # This way we could possibly get rid of reflection and set kind of symbol table to pipeline level where extractors could check
+            # the match locations of others when required.
+            if ext[0]['set_dependency_match_position_to_zero']:
+                ext[1].setDependencyMatchPositionToZero()
+
+            if ext[0]['depends_on_match_position_of_extractor'] is not None:
+                dependency_extractor = self._find_extractor_by_class_name(ext[0]['depends_on_match_position_of_extractor'], extractors)
+                ext[1].dependsOnMatchPositionOf(dependency_extractor)
+
+            extraction_results.update(ext[1].extract(entry['text'], entry))
+
+        extraction_results["originalText"] = entry['text']  #FIXME: Make own extractor for this. Not a responsibility of this class.
+        return extraction_results
+
+
+def configure_extractor(extractor_class, depends_on_match_position_of_extractor=None, set_dependency_match_position_to_zero=False):
+    """
+    Utility function to build configure dict object for extraction pipeline.
+    :param extractor_class:
+    :param depends_on_match_position_of_extractor: Extractor class this extractor is dependent on their match position.
+    :param set_dependency_match_position_to_zero:
+    :return:
+    """
+    config = {
+        'extractor_class': extractor_class,
+        'set_dependency_match_position_to_zero': set_dependency_match_position_to_zero,
+        'depends_on_match_position_of_extractor': depends_on_match_position_of_extractor
+    }
+
+    return config
--- book_extractors/karelians/main.py
+++ book_extractors/karelians/main.py
@@ -1,15 +1,44 @@
 from book_extractors.processdata import ProcessData
 from book_extractors.karelians.resultcsvbuilder import ResultCsvBuilder
 from book_extractors.karelians.resultjsonbuilder import ResultJsonBuilder
-from book_extractors.karelians.extraction.extractionPipeline import ExtractionPipeline
+from book_extractors.extraction_pipeline import ExtractionPipeline, configure_extractor
+from book_extractors.karelians.extraction.extractors.nameextractor import NameExtractor
+from book_extractors.karelians.extraction.extractors.origfamilyextractor import OrigFamilyExtractor
+from book_extractors.karelians.extraction.extractors.professionextractor import ProfessionExtractor
+from book_extractors.karelians.extraction.extractors.imageextractor import ImageExtractor
+from book_extractors.karelians.extraction.extractors.omakotitaloextractor import OmakotitaloExtractor
+from book_extractors.karelians.extraction.extractors.birthdayExtractor import BirthdayExtractor
+from book_extractors.karelians.extraction.extractors.locationExtractor import BirthdayLocationExtractor
+from book_extractors.karelians.extraction.extractors.migration_route_extractors import FinnishLocationsExtractor, KarelianLocationsExtractor
+from book_extractors.karelians.extraction.extractors.spouseextractor import SpouseExtractor
+from book_extractors.karelians.extraction.extractors.childextractor import ChildExtractor
+from shared.genderExtract import Gender
 
 
 class KarelianExtractor:
 
     def __init__(self, update_callback):
-        self._extractor_pipeline = ExtractionPipeline()
+        Gender.load_names()
+        self._extractor_pipeline = self._define_extraction_pipeline()
         self._extractor = ProcessData(self._extractor_pipeline, update_callback)
         self._results = None
+
+    def _define_extraction_pipeline(self):
+        pipeline_components = [
+            configure_extractor(NameExtractor),
+            configure_extractor(ImageExtractor),
+            configure_extractor(OrigFamilyExtractor, set_dependency_match_position_to_zero=True),
+            configure_extractor(ProfessionExtractor, depends_on_match_position_of_extractor=OrigFamilyExtractor),
+            configure_extractor(BirthdayExtractor, depends_on_match_position_of_extractor=OrigFamilyExtractor),
+            configure_extractor(BirthdayLocationExtractor, depends_on_match_position_of_extractor=BirthdayExtractor),
+            configure_extractor(KarelianLocationsExtractor),
+            configure_extractor(FinnishLocationsExtractor),
+            configure_extractor(OmakotitaloExtractor),
+            configure_extractor(SpouseExtractor),
+            configure_extractor(ChildExtractor)
+        ]
+
+        return ExtractionPipeline(pipeline_components)
 
     def process(self, person_data):
         self._results = self._extractor.run_extraction(person_data)
