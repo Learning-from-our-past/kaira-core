d938cbb4fb7bf1cb9897a387e69d0a9f5c7c9305
Tuomas Salmi <salmi.tuomas@gmail.com>
2017-03-11 13:30:25 +0200

    Remove useless stuff from BaseExtractor

--- book_extractors/common/base_extractor.py
+++ book_extractors/common/base_extractor.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from abc import ABCMeta, abstractmethod
-import re
 
 
 class BaseExtractor():
@@ -8,12 +7,9 @@ class BaseExtractor():
 
     def __init__(self, current_person, options=None):
         self._sub_extraction_pipeline = None
-        self.regexPattern = r''
-        self.currentChild = None
         self.REQUIRES_MATCH_POSITION = False    # Set this to true in subclass if you want to enforce dependsOnMatchPositionOf() before extract()
-        self.matchStartPosition = 0            # position in string where to begin match. Only used on certain classes
+        self.matchStartPosition = 0             # position in string where to begin match. Only used on certain classes
         self.matchFinalPosition = 0             # after extractor is finished, save the ending position of the match
-        self.currentChild = current_person
 
     @abstractmethod
     def extract(self, text, person):
@@ -25,11 +21,6 @@ class BaseExtractor():
     def dependsOnMatchPositionOf(self, extractor):
         self.matchStartPosition = extractor.getFinalMatchPosition()
         self.matchFinalPosition = self.matchStartPosition   # in case extractor doesn't find anything, we set finalPos as startPos
-
-    #TODO: REMOVE AND USE regexUtils
-    def _executeSearchRegex(self, text, options=re.UNICODE | re.IGNORECASE):
-        r = re.compile(self.regexPattern, options)
-        return r.search(text)
 
     @abstractmethod
     def _constructReturnDict(self):
