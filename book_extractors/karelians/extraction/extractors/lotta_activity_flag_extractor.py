f15192653b7f1983228c67b2e2a97402ec727fa2
Juuso Kallioniemi <jkallioniemi@gmail.com>
2017-10-05 15:22:44 +0300

    Write logic for lotta activity flag extractor

--- book_extractors/karelians/extraction/extractors/lotta_activity_flag_extractor.py
+++ book_extractors/karelians/extraction/extractors/lotta_activity_flag_extractor.py
@@ -1,4 +1,6 @@
 from book_extractors.common.extractors.base_extractor import BaseExtractor
+from shared.textUtils import remove_hyphens_from_text
+import regex
 
 
 class LottaActivityFlagExtractor(BaseExtractor):
@@ -6,6 +8,9 @@ class LottaActivityFlagExtractor(BaseExtractor):
 
     def __init__(self, key_of_cursor_location_dependent, options):
         super(LottaActivityFlagExtractor, self).__init__(key_of_cursor_location_dependent, options)
+        self.OPTIONS = regex.UNICODE
+        self.LOTTA_ACTIVITY_PATTERN = r'(?:(?<=l)otta|(L|l)otta(?=\s(S|s)värd)){s<=1}'
+        self.REGEX_LOTTA_ACTIVITY = regex.compile(self.LOTTA_ACTIVITY_PATTERN, self.OPTIONS)
 
     def _extract(self, entry, extraction_results, extraction_metadata):
         lotta_activity = self._check_lotta_activity(entry['text'])
@@ -13,4 +18,6 @@ class LottaActivityFlagExtractor(BaseExtractor):
         return self._add_to_extraction_results(lotta_activity, extraction_results, extraction_metadata)
 
     def _check_lotta_activity(self, text):
-        return None
+        text = remove_hyphens_from_text(text)
+        lotta = regex.search(self.REGEX_LOTTA_ACTIVITY, text)
+        return lotta is not None
