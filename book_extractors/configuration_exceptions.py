03d89c8a4ddf6ed47a74967a47d370ab7ce06fc9
Lumi Kallioniemi <kallioniemi@pm.me>
2017-10-10 19:55:23 +0300

    Implement dependency resolving
    The framework now automatically resolves dependencies that are
    declared and place the results of those extractors under the variable
    self._dependee_results.

--- book_extractors/configuration_exceptions.py
+++ book_extractors/configuration_exceptions.py
@@ -11,3 +11,16 @@ class DependencyConfigurationException(ConfigurationException):
                     'Please explicitly enter a context for each dependency. '
                     'The following extractors are missing a context: {0}'.format(self.missing_contexts))
     pass
+
+
+class ContextKeywordConfigurationException(ConfigurationException):
+    def __str__(self):
+        return repr('Something is wrong with your dependency contexts! The context has to be either '
+                    'current, main or a string of however many parents you want, separated by dots.')
+
+
+class ParentKeywordConfigurationException(ConfigurationException):
+    def __str__(self):
+        return repr('Reached main pipeline before reaching the parent pipeline configured in the '
+                    'dependency\'s contexts. This probably means there are too many \'parent\' '
+                    'or that parent_data is not being passed correctly to subpipelines.')
