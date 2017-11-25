59d4dddb6dfe8d56f6bd12696493a6ee36302567
Tuomas Salmi <salmi.tuomas@gmail.com>
2017-11-25 22:59:17 +0200

    Change " -> '

--- book_extractors/karelians/extraction/extractors/tests/children/mock_person_data.py
+++ book_extractors/karelians/extraction/extractors/tests/children/mock_person_data.py
@@ -1,7 +1,7 @@
 CHILDREN_TEXTS = [
-    "o.s. Testinen, rouva, synt. 19. 7. -16 Hiitolassa. Puol. Mies Miehekäs, ahtaaja, synt. 12. 2."
-    "    17Ahlaisissa. Avioit. -44. Lapset: Irja Hellevi -45, Kirsti Anna-Liisa -47, Kalervo Viljam -53. "
-    "Syntyneet Ahlaisissa. Asuinp. Karjalassa: Hiitola. Päijälä 40. 43—44."
+    'o.s. Testinen, rouva, synt. 19. 7. -16 Hiitolassa. Puol. Mies Miehekäs, ahtaaja, synt. 12. 2.'
+    '    17Ahlaisissa. Avioit. -44. Lapset: Irja Hellevi -45, Kirsti Anna-Liisa -47, Kalervo Viljam -53. '
+    'Syntyneet Ahlaisissa. Asuinp. Karjalassa: Hiitola. Päijälä 40. 43—44.'
 ]
 
 EXPECTED = [
