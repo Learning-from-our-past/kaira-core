d75f536e1ec6cfcfbcf2916931d70d855de46910
Tuomas Salmi <salmi.tuomas@gmail.com>
2018-01-26 10:44:29 +0200

    Simplify yaml with custom constructor
    Refactored yaml parser to use a custom constructor which
    dynamically imports and instantiates extractor classes. This
    way we have more control on the format of the yaml file which
    is nice since the default syntax for class instantiation with
    PyYaml is kinda messy.

--- pipeline_creation/yaml_parser.py
+++ pipeline_creation/yaml_parser.py
@@ -1,5 +1,25 @@
 import yaml
+import importlib
 from book_extractors.extraction_pipeline import ExtractionPipeline
+
+
+def extractor_constructor(loader, node):
+    """
+    Define a custom PyYaml constructor to instantiate pipeline extractors. Imports
+    classes and modules dynamically based on names and paths in yaml.
+    """
+    extractor_params = loader.construct_mapping(node, deep=True)
+
+    extractor_module = importlib.import_module(extractor_params['module'])
+    extractor_class = getattr(extractor_module, extractor_params['class_name'])
+
+    cursor_location_dependence = extractor_params['cursor_location_depend_on'] if 'cursor_location_depend_on' in extractor_params else None
+    options = extractor_params['options'] if 'options' in extractor_params else None
+
+    return extractor_class(cursor_location_dependence, options)
+
+
+yaml.add_constructor('!Extractor', extractor_constructor)
 
 
 def parse_config(path):
