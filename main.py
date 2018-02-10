2b08845c0cec992b2a88150b16f4b370a7909006
Juuso Kallioniemi <jkallioniemi@gmail.com>
2018-02-10 20:47:13 +0200

    Make progress bar prettier

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ supported_bookseries = {
 }
 
 help_str = 'Bookseries where data is from: {}'.format(', '.join(list(supported_bookseries.keys())))
-progress_str = 'Progress: {}% - {}/{}'
+progress_str = 'Progress:{:>4}% - {:>5}/{:>5}'
 progress_bar_str = '{} [{}{}] \r'
 
 parser = argparse.ArgumentParser(description='Extract information from matrikel books.')
