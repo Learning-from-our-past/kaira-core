748335ad8d6885e2e2311e81df0b36e57676bfa8
Tuomas Salmi <salmi.tuomas@gmail.com>
2018-02-17 12:02:53 +0200

    Use YAML config on small farmers extraction
    Produces same results as the old implementation. Changed some names
    of the boolean flags though. Some had uppercase first letter and finnish names
    for no reason.

--- book_extractors/farmers/extraction/extractors/owner_extractor.py
+++ book_extractors/farmers/extraction/extractors/owner_extractor.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from book_extractors.common.extractors.owner_extractor import CommonOwnerExtractor
-from book_extractors.farmers.extraction.extractors.birthday_extractor import BirthdayExtractor
 from book_extractors.common.extraction_keys import KEYS
 
 
@@ -13,8 +12,7 @@ class OwnerExtractor(CommonOwnerExtractor):
 
         options.update({
             'OWNER_YEAR_PATTERN': r"om(?:\.|,)\s?vuodesta\s(?P<year>\d\d\d\d)",
-            'OWNER_NAME_PATTERN': r"(?P<name>[A-ZÄ-Öa-zä-ö -]+(?:o\.s\.)?[A-ZÄ-Öa-zä-ö -]+)(?:\.|,)\ssynt",
-            'BIRTHDAY_EXTRACTOR': BirthdayExtractor
+            'OWNER_NAME_PATTERN': r"(?P<name>[A-ZÄ-Öa-zä-ö -]+(?:o\.s\.)?[A-ZÄ-Öa-zä-ö -]+)(?:\.|,)\ssynt"
         })
 
         super(OwnerExtractor, self).__init__(cursor_location_depend_on, options)
