6c99e66e38326ad15e87b96ad9d37b53b92b080f
Tuomas Salmi <salmi.tuomas@gmail.com>
2018-02-17 14:11:51 +0200

    Remove old extractor configuration
    Not used anymore since YAML handles everything.

--- book_extractors/extraction_pipeline.py
+++ book_extractors/extraction_pipeline.py
@@ -36,37 +36,3 @@ class ExtractionPipeline:
                                                                  parent_pipeline_data=parent_pipeline_data)
 
         return extraction_output, extraction_metadata
-
-
-def configure_extractor(extractor_class, dependencies_contexts=None, extractor_options=None, path=None, depends_on_match_position_of_extractor=None):
-    """
-    Utility function to build configure dict object for extraction pipeline.
-    :param extractor_class:
-    :param dependencies_contexts: Used to find the location (pipelinewise) where the extractor or extractors that we
-                                  depend on reside. The extractor needs this information to get the extraction results
-                                  its operation depends on.
-    :param extractor_options: Possible kwargs arguments which can be passed to extractor. Some extractors might need
-                              arbitrary extra parametes in their __init__
-    :param path: Path of the extractor's results in the dict and json output. Can be used to define parent groups for
-                 the output of the extractor.
-    :param depends_on_match_position_of_extractor: Extractor class this extractor is dependent on their match position.
-    :return:
-    """
-
-    depends_on = None
-    if depends_on_match_position_of_extractor is not None:
-        depends_on = depends_on_match_position_of_extractor
-
-    if extractor_options is not None:
-        extractor_options['output_path'] = path
-    else:
-        extractor_options = {'output_path': path}
-
-    config = {
-        'extractor_class': extractor_class,
-        'cursor_location_depend_on': depends_on,
-        'extractor_options': extractor_options,
-        'dependencies_contexts': dependencies_contexts
-    }
-
-    return config
