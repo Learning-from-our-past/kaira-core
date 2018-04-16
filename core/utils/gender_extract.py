34329c74a5b1f2490611a04f25598334eebd98c2
Lumi Kallioniemi <kallioniemi@pm.me>
2018-04-16 16:40:45 +0300

    Determine sex from number of sex specific names
    The old method of determining a person's sex only looked at
    their first name, gave up trying to find the person's sex
    as soon as it determined the person is female. This led to
    at least 3000, at most 6000 people being assigned the
    incorrect sex.
    
    The new implementation looks at how many male and how many
    female names the person has and tries to determine sex from
    that instead.

--- core/utils/gender_extract.py
+++ core/utils/gender_extract.py
@@ -1,6 +1,3 @@
-import re
-
-
 class Gender():
     male_names = []
     female_names = []
@@ -24,17 +21,36 @@ class Gender():
             Gender.female_names = set(Gender.female_names)
 
     @staticmethod
-    def find_gender(name):
-        firstname = re.search(r"(?P<name>^\w+)", name.lower(), re.UNICODE | re.IGNORECASE)
-        try:
-            if firstname.group("name") in Gender.male_names:
-                return "Male"
-            elif firstname.group("name") in Gender.female_names:
-                return "Female"
-            else:
-                raise GenderException()
-        except AttributeError:
-            return ""
+    def find_gender(name_string):
+        """
+        Tries to determine sex from a string of name(s).
+        :param name_string: A string containing one or more names.
+        :return: 'Male', 'Female' or None
+        """
+        names = tuple(word.casefold() for word in name_string.split(' ') if word)
+        num_male_names, num_female_names = Gender._count_sex_specific_names(names)
+        sex = None
+        if num_male_names > num_female_names:
+            sex = 'Male'
+        elif num_female_names > num_male_names:
+            sex = 'Female'
+        elif num_male_names == 0 and num_female_names == 0 and len(names) >= 1:
+            raise GenderException()
+
+        return sex
+
+    @staticmethod
+    def _count_sex_specific_names(names):
+        num_male_names = 0
+        num_female_names = 0
+
+        for name in names:
+            if name in Gender.male_names:
+                num_male_names += 1
+            elif name in Gender.female_names:
+                num_female_names += 1
+
+        return num_male_names, num_female_names
 
 
 class GenderException(Exception):
