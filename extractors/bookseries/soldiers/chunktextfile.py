33485ded9e2abd0fede1f8cf522feafd4d3e1852
Tuomas Salmi <salmi.tuomas@gmail.com>
2018-04-19 20:57:53 +0300

    Chunk Suomen Rintamamiehet from html to xml format
    A minimal implementation. Parses the html and splits the persons by
    paragraphs which begin with at least three uppercase letters. This will
    likely produce at least some incorrect results but that is fine for
    now since this is a proof of concept of adding a new book series to the
    Kaira. If more accuracy is needed one should improve the chunker logic
    by writing a bit more sophisticated heuristics.

--- extractors/bookseries/soldiers/chunktextfile.py
+++ extractors/bookseries/soldiers/chunktextfile.py
@@ -10,6 +10,11 @@ def read_html_file(path):
 
 
 class PersonPreprocessor(ChunkTextInterface):
+    """
+    This is a proof of concept chunker and therefore is not optimized to handle all corner cases which
+    will appear in ABBYY html-files such as cases where another person entry begins "inside" previous
+    person's entry.
+    """
     def __init__(self, bookseries_id):
         super(PersonPreprocessor, self).__init__(bookseries_id)
         self._save_path = None
@@ -18,10 +23,15 @@ class PersonPreprocessor(ChunkTextInterface):
         self._page_number = None
         self._current_person = None
 
+        # A regex which is used to identify <p> tag which begins a new person entry. extremely naive since it
+        # just assumes that if the tag begins with three uppercase letters, the paragraph begins a new entry.
+        self._ENTRY_BEGINS_REGEX = re.compile(r'^[A-ZÄÖ]{3,}', re.UNICODE)
+
     def chunk_text(self, text, destination_path, book_number):
         self._save_path = destination_path
         self._book_number = book_number
-        text = re.sub(r'(\<sup\>)|\<\/sup\>', '', text)
+        text = re.sub(r'(<sup>)|</sup>', '', text)
+        text = re.sub(r'&nbsp;', ' ', text)
         parsed = html.document_fromstring(text)
         persons = self._process(parsed)
         return etree.tostring(persons, pretty_print=True, encoding='unicode')
@@ -38,9 +48,42 @@ class PersonPreprocessor(ChunkTextInterface):
 
     def _walk_tree(self, tree):
         for e in tree.iter():
-            if e.text is not None:
-                print(e.text)
+            if 'src' in e.attrib:
+                # We are not interested in images for now
+                continue
 
+            if e.text is not None:
+                try:
+                    self._page_number = int(e.text)
+                except ValueError:
+                    if self._ENTRY_BEGINS_REGEX.search(e.text) is not None:
+                        self._add_person(self._current_person)
+                        self._current_person = self._create_person(e.text)
+                    elif self._current_person is not None:
+                        self._add_element_to_person(e)
+
+        self._add_person(self._current_person)
+        return self._persons_document
+
+    def _add_element_to_person(self, e):
+        if len(e.text) > 40:
+            # TODO: Here we could try to identify person entries which begin midst of the current person entry
+            # See a reference implementation in siirtokarjalaisten tie chunker
+
+            # For now just concatenate the element's text to the current person
+            self._current_person.text += e.text
+            self._current_person.text = re.sub('\n', ' ', self._current_person.text)
+            self._current_person.text = re.sub('\s{2,4}', ' ', self._current_person.text)
+
+    def _create_person(self, entry):
+        person = etree.Element('PERSON')
+        person.attrib['approximated_page'] = '{}-{}'.format(self._page_number - 1, self._page_number + 1)
+        person.text = entry
+        return person
+
+    def _add_person(self, person):
+        if person is not None and len(person.text) > 4:
+            self._persons_document.append(person)
 
 
 def convert_html_file_to_xml(bookseries_id, input_file, output_file, book_number, filter_duplicates=False, callback=None):
